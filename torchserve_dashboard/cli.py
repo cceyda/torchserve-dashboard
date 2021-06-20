bbc3e00064cfb7a549690716800393d0e5ff9962
Florian Muller-Fouarge <florian.mueller.fouarge@gmail.com>
2021-06-20 17:50:02 +0200

    add typing to cli.py

--- torchserve_dashboard/cli.py
+++ torchserve_dashboard/cli.py
@@ -1,17 +1,16 @@
+from typing import Any
 import click
 import streamlit.cli
 from streamlit.cli import configurator_options
 import os
 
-@click.command(context_settings=dict(
-    ignore_unknown_options=True,
-    allow_extra_args = True
-))
+
+@click.command(context_settings=dict(ignore_unknown_options=True,
+                                     allow_extra_args=True))
 @configurator_options
 @click.argument("args", nargs=-1)
 @click.pass_context
-def main(ctx,args,**kwargs):
+def main(ctx: click.Context, args: Any, **kwargs: Any):
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'dash.py')
     ctx.forward(streamlit.cli.main_run, target=filename, args=args, *kwargs)
-
